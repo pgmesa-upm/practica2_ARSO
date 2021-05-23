9aba167cf9ad5acb05ea2700ad2cfd37805d17fb
pablo <pablogmesa00@gmail.com>
2021-05-23 17:10:05 +0200

    Añadiendo correccion al eliminar desde fuer aun bridge y querer añadir servidor o cliente despues

--- program/platform/platform.py
+++ program/platform/platform.py
@@ -61,15 +61,35 @@ def update_conexions():
         lxdbr0_exists = bgs_dict.get("lxdbr0", None) is not None
         lxdbr1_exists = bgs_dict.get("lxdbr1", None) is not None
         if c.tag == load_balancer.TAG:
-            if not c.connected_networks["eth0"] and lxdbr0_exists:
+            if not c.connected_networks["eth0"]:
+                if not lxdbr0_exists:
+                    err = (f"Error al conectar {c.tag} '{c.name}' a "
+                             + "bridge lxdbr0. Este bridge no existe")
+                    plt_logger.error(err)
+                    continue
                 bridges.attach(c.name, bgs_dict["lxdbr0"], "eth0")
-            if not c.connected_networks["eth1"] and lxdbr1_exists:
+            if not c.connected_networks["eth1"]:
+                if not lxdbr1_exists:
+                    err = (f"Error al conectar {c.tag} '{c.name}' a "
+                             + "bridge lxdbr1. Este bridge no existe")
+                    plt_logger.error(err)
+                    continue
                 bridges.attach(c.name, bgs_dict["lxdbr1"], "eth1")
         elif c.tag == servers.TAG or c.tag == data_base.TAG:
-            if not c.connected_networks["eth0"] and lxdbr0_exists:
+            if not c.connected_networks["eth0"]:
+                if not lxdbr0_exists:
+                    err = (f"Error al conectar {c.tag} '{c.name}' a "
+                             + "bridge lxdbr0. Este bridge no existe")
+                    plt_logger.error(err)
+                    continue
                 bridges.attach(c.name, bgs_dict["lxdbr0"], "eth0")
         elif c.tag == client.TAG:
-            if not c.connected_networks["eth0"] and lxdbr1_exists:
+            if not c.connected_networks["eth0"]:
+                if not lxdbr1_exists:
+                    err = (f"Error al conectar {c.tag} '{c.name}' a "
+                             + "bridge lxdbr1. Este bridge no existe")
+                    plt_logger.error(err)
+                    continue
                 bridges.attach(c.name, bgs_dict["lxdbr1"], "eth0")
         containers.connect_to_networks(c)
     register.update("updates", {})
