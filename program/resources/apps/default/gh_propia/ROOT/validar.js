5795b897a2e449812560091f7b68f2de60b49080
pablo <pablogmesa00@gmail.com>
2021-06-03 20:40:50 +0200

    arreglando fallo raro en gh_propia

--- program/resources/apps/default/gh_propia/ROOT/validar.js
+++ program/resources/apps/default/gh_propia/ROOT/validar.js
@@ -1,26 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let botAlta = document.getElementById('boton_alta');
 
-let botAlta = document.getElementById('boton_alta');
-
-botAlta.onclick =  function validar() {
-	
-    let numero, letra, letras, formato, dni;
-    letras = 'TRWAGMYFPDXBNJZSQVHLCKET';
-    formato = /^\d{8}[a-zA-Z]$/;
-    dni = document.forms["form_alta"]["DNI_ingresado"].value;
-    if(formato.test(dni)){
-        numero = dni.substr(0, dni.length-1);
-        letra = dni.substr(dni.length-1, 1);
-        numero = numero % 23;
-        letras=letras.substring(numero, numero+1);
-        if (letras!=letra) {
-            alert("Letra errónea");
+    botAlta.onclick =  function validar() {
+        
+        let numero, letra, letras, formato, dni;
+        letras = 'TRWAGMYFPDXBNJZSQVHLCKET';
+        formato = /^\d{8}[a-zA-Z]$/;
+        dni = document.forms["form_alta"]["DNI_ingresado"].value;
+        if(formato.test(dni)){
+            numero = dni.substr(0, dni.length-1);
+            letra = dni.substr(dni.length-1, 1);
+            numero = numero % 23;
+            letras=letras.substring(numero, numero+1);
+            if (letras!=letra) {
+                alert("Letra errónea");
+                return false;
+            }
+            else
+            return true;
+        } else {
+            
+            alert("Formato erróneo");
             return false;
         }
-        else
-        return true;
-    } else {
-        
-        alert("Formato erróneo");
-        return false;
     }
-}
+})
